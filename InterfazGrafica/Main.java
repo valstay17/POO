e0ff546eb52117170f2a792b622bd47b05027efa
Valeria Carmona Lopez <valeriacarmona92@aragon.unam.mx>
2023-05-02 12:17:26 -0600

    Tarea 9

---
+++ InterfazGrafica/Main.java
@@ -0,0 +1,9 @@
+package InterfazGrafica;
+
+import InterfazGrafica.iu.Principal;
+
+public class Main {
+    public static void main(String[] args) {
+        Principal ventana = new Principal("Hola mundo AWT");
+    }
+}
