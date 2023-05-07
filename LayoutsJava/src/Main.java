82043b98230aedade4068c86506359d3ca1139e3
Valeria Carmona Lopez <valeriacarmona92@aragon.unam.mx>
2023-05-06 21:41:49 -0600

    Actividad extra 3

---
+++ LayoutsJava/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        //Ventana ventana = new Ventana("Layouts");
+        VentanaGrid ventanaGrid = new VentanaGrid("Ventana Layout cuadricula");
+
+    }
+}
