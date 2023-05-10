3a601534171b3493448d1655fc4cd17c0734feaa
Valeria Carmona Lopez <valeriacarmona92@aragon.unam.mx>
2023-05-10 15:19:53 -0600

    Tarea 11

---
+++ tarea11/Main.java
@@ -0,0 +1,8 @@
+package tarea11;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Ventana ventana = new Ventana("ComboBox MVC");
+    }
+}
